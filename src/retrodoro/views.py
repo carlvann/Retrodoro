44cf4ea9db881bd3c0faf93561e7e94cb5046aa5
Kelly-Ann Downer <kelly-anndowner@MacBookPro.lan>
2025-01-02 02:04:17 -0500

    stuff

--- src/retrodoro/views.py
+++ src/retrodoro/views.py
@@ -1,4 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .utils.db_util import user_exists, add_user
+
 
 def login(request):
+    return render(request, 'retrodoro/login.html')
+
+def loginuser(request):
+    if request.POST:
+        request_data = request.POST.dict()
+        username = request_data.get("username")
+        password = request_data.get("password")
+        if not user_exists:
+            add_user(username, password)
+            return render(request, 'retrodoro/login.html')
+
     return render(request, 'retrodoro/login.html')
